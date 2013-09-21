30842ea8a8dae97a26831f254362aadb7aea30a6
Simon Sickle <simonsimons34@gmail.com>
2013-09-21 21:20:53 +0000

    Get ready for 4.3

--- device_k2_ul.mk
+++ device_k2_ul.mk
@@ -11,10 +11,11 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 PRODUCT_COPY_FILES += \
-    device/htc/k2_ul/recovery/sbin/choice_fn:recovery/root/sbin/choice_fn \
-    device/htc/k2_ul/recovery/sbin/detect_key:recovery/root/sbin/detect_key \
-    device/htc/k2_ul/recovery/sbin/offmode_charging:recovery/root/sbin/offmode_charging \
-    device/htc/k2_ul/recovery/sbin/power_test:recovery/root/sbin/power_test
+    device/htc/k2_ul/recovery/choice_fn:recovery/root/sbin/choice_fn \
+    device/htc/k2_ul/recovery/detect_key:recovery/root/sbin/detect_key \
+    device/htc/k2_ul/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    device/htc/k2_ul/recovery/offmode_charging:recovery/root/sbin/offmode_charging \
+    device/htc/k2_ul/recovery/power_test:recovery/root/sbin/power_test
 
 PRODUCT_NAME := One SV
 PRODUCT_DEVICE := k2_ul
